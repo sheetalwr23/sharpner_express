c88a24c022ec962cb12d6a61dbd56d9170b9c6a1
Sheetal <Sheetalwr23@gmail.com>
2023-07-24 13:53:26 +0530

    task 2 is done

--- task2.js
+++ task2.js
@@ -1,13 +1,24 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+
 // const server = http.createServer(app);
-app.use((req, res, next) => {
-  console.log("In the first middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
-app.use((req, res, next) => {
-  console.log("In the second middleware");
-  res.send("<h1>Hello from express<h1>");
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
+
 app.listen(3000);
